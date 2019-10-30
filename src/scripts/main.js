d553ba9557ee3763ad828344e6502e7b0111dac1
Caroline Brownlee <brownleecaroline@gmail.com>
2019-10-29 21:33:21 -0500

    commented out code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,9 @@ const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uran
     Use string templates to construct the DOM elements.
 */
 
+// grabs id from <section> container in index.html
 const planetEl = document.getElementById("planets")
+// forEach loops through the plants array and for each planet, puts that planet on the dom 
 planets.forEach(planet => {
     planetEl.innerHTML += 
     `<section>${planet}</section>`
@@ -19,8 +21,11 @@ first letter of each planet is capitalized. Use the
 
 */
 
+// maps through the planet array, 
 const planet = planets.map(planet => {
+    // returns each planet's upperCast first letter (charAt(0) = the index of the first character in a string (index starts with 0)),
     return planet.charAt(0).toUpperCase() 
+    // and adds the rest of the letters for each planet starting with the second character. Second character = (1).
     + planet.slice(1)
 })
 console.log(planet)
@@ -33,5 +38,6 @@ console.log(planet)
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
 
+// filters through the planets array and for each planet checks to see if it includes the letter "e". 
 const planetIncludesE = planets.filter(planet => planet.includes("e"))
 console.log(planetIncludesE)
